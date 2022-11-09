0b8a85953b23641dcce84428a6ce625b9c892636
LucasIvanLezcano <lordvaldomero47@gmail.com>
2022-11-09 15:29:21 -0300

    actualizacion_2

--- my_blog/movie/models.py
+++ my_blog/movie/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 
 
 class Movie(models.Model):
@@ -7,9 +9,31 @@ class Movie(models.Model):
     release_date = models.DateField()
     produced_by = models.CharField(max_length=40)
     description = RichTextField(null=True, blank=True)
+    comments = models.ManyToManyField(
+        User, through="Comment", related_name="comments_owned"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = (
+            "name",
+            "produced_by",
+        )
+        ordering = ["-created_at"]
 
     def __str__(self):
-        return f"Name: {self.name} - Release_date: {self.release_date} - Produced_by: {self.produced_by}"
-    
+        return f"Movie: {self.name} | produced_by: {self.produced_by}"
 
+        
+class Comment(models.Model):
+    text = models.TextField(
+        validators=[
+            MinLengthValidator(10, "El comentario debe ser mayor de 10 caracteres")
+        ]
+    )
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
--- my_blog/movie/views.py
+++ my_blog/movie/views.py
@@ -1,10 +1,101 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
 from django.contrib import messages
-from django.forms.models import model_to_dict
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import ValidationError
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from movie.models import Movie
 from movie.forms import MovieForm
+
+
+class MovieListView(ListView):
+    model = Movie
+    paginate_by = 3
+
+
+class MovieDetailView(DetailView):
+    model = Movie
+    template_name = "movie/movie_detail.html"
+    fields = ["name", "code", "description"]
+
+    def get(self, request, pk):
+        movie = Movie.objects.get(id=pk)
+        comments = Comment.objects.filter(movie=movie).order_by("-updated_at")
+        comment_form = CommentForm()
+        context = {
+            "movie": movie,
+            "comments": comments,
+            "comment_form": comment_form,
+        }
+        return render(request, self.template_name, context)
+
+
+class MovieCreateView(LoginRequiredMixin, CreateView):
+    model = Movie
+    success_url = reverse_lazy("movie:movie-list")
+
+    form_class = MovieForm
+    # fields = ["name", "code", "description"]
+
+    def form_valid(self, form):
+        """Filter to avoid duplicate movies"""
+        data = form.cleaned_data
+        form.instance.owner = self.request.user
+        actual_objects = Movie.objects.filter(
+            name=data["name"], code=data["code"]
+        ).count()
+        if actual_objects:
+            messages.error(
+                self.request,
+                f"El curso {data['name']} - {data['code']} ya está creado",
+            )
+            form.add_error("name", ValidationError("Acción no válida"))
+            return super().form_invalid(form)
+        else:
+            messages.success(
+                self.request,
+                f"Curso {data['name']} - {data['code']} creado exitosamente!",
+            )
+            return super().form_valid(form)
+
+
+class MovieUpdateView(LoginRequiredMixin, UpdateView):
+    model = Movie
+    fields = ["name", "code", "description"]
+
+    def get_success_url(self):
+        movie_id = self.kwargs["pk"]
+        return reverse_lazy("movie:movie-detail", kwargs={"pk": movie_id})
+
+
+class MovieDeleteView(LoginRequiredMixin, DeleteView):
+    model = Movie
+    success_url = reverse_lazy("movie:movie-list")
+
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    def post(self, request, pk):
+        movie = get_object_or_404(Movie, id=pk)
+        comment = Comment(
+            text=request.POST["comment_text"], owner=request.user, movie=movie
+        )
+        comment.save()
+        return redirect(reverse("movie:movie-detail", kwargs={"pk": pk}))
+
+
+class CommentDeleteView(LoginRequiredMixin, DeleteView):
+    model = Comment
+
+    def get_success_url(self):
+        movie = self.object.movie
+        return reverse("movie:movie-detail", kwargs={"pk": movie.id})
+
+
+
+
 
 
 def movies(request):
@@ -137,9 +228,50 @@ from movie.models import Movie
 class MovieListView(ListView):
     model: Movie
     paginate_by = 3
-    template_name = "movie/movie_list.html"
 
-class MovieDetailView(DeleteView):
+
+class MovieDetailView(DetailView):
     model = Movie
     template_name = "movie/movie_detail.html"
     fields = ["name", "release_date", "produced_by", "description",]
+
+class MovieCreateView(CreateView):
+    model = Movie
+    success_url = reverse_lazy("movie:movie-list")
+
+    form_class = MovieForm
+    # fields = ["name", "release_date", "produced_by", "description"]
+
+    def form_valid(self, form):
+        """Filter to avoid duplicate movies"""
+        data = form.cleaned_data
+        actual_objects = Movie.objects.filter(
+            name=data["name"], reselase_date=data["reselase_date"], produced_by=data["produced_by"],
+        ).count()
+        if actual_objects:
+            messages.error(
+                self.request,
+                f"La pelicula {data['name']} - {data['reselase_date']} - {data['produced_by']} ya está creado",
+            )
+            form.add_error("name", ValidationError("Acción no válida"))
+            return super().form_invalid(form)
+        else:
+            messages.success(
+                self.request,
+                f"movie {data['name']} - {data['reselase_date']} - {data['produced_by']} creado exitosamente!",
+            )
+            return super().form_valid(form)
+
+
+class MovieUpdateView(UpdateView):
+    model = Movie
+    fields = ["name", "reselase_date", "produced_by", "description"]
+
+    def get_success_url(self):
+        movie_id = self.kwargs["pk"]
+        return reverse_lazy("movie:movie-detail", kwargs={"pk": movie_id})
+
+
+class MovieDeleteView(DeleteView):
+    model = Movie
+    success_url = reverse_lazy("movie:movie-list")
